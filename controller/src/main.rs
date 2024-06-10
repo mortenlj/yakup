137537df2cf669fd44c18e8cbdc1e51e0d49d1eb
Morten Lied Johansen <mortenjo@ifi.uio.no>
2024-06-10 21:21:33 +0200

    Minimal Application controller

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+use std::time::Duration;
+
+use futures::StreamExt;
+use kube::{Api, Client, ResourceExt};
+use kube::runtime::controller::Action;
+use kube::runtime::controller::Controller;
+use tokio;
+
+use api::Application;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+
+#[tokio::main]
+async fn main() -> Result<(), kube::Error> {
+    let client = Client::try_default().await?;
+    let pods = Api::<Application>::all(client);
+
+    Controller::new(pods.clone(), Default::default())
+        .run(reconcile, error_policy, Arc::new(()))
+        .for_each(|_| futures::future::ready(()))
+        .await;
+
+    Ok(())
+}
+
+async fn reconcile(obj: Arc<Application>, _ctx: Arc<()>) -> Result<Action> {
+    println!("reconcile request: {}", obj.name_any());
+    Ok(Action::requeue(Duration::from_secs(3600)))
+}
+
+fn error_policy(_object: Arc<Application>, _err: &Error, _ctx: Arc<()>) -> Action {
+    Action::requeue(Duration::from_secs(5))
 }
