7e4188c029c24312958f7dbf8bc0e9143739f344
Morten Lied Johansen <mortenjo@ifi.uio.no>
2024-06-27 15:26:11 +0200

    Set proper names and lables

--- controller/src/resource_creator/deployment.rs
+++ controller/src/resource_creator/deployment.rs
@@ -1,8 +1,8 @@
+use std::collections::BTreeMap;
 use std::sync::Arc;
 use k8s_openapi::api::apps::v1::Deployment;
 use k8s_openapi::api::core::v1::PodTemplateSpec;
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::{LabelSelector, ObjectMeta};
-use kube::api::DynamicObject;
 use kube::ResourceExt;
 use tracing::instrument;
 
@@ -13,38 +13,34 @@ use crate::resource_creator::to_dynamic_object;
 use crate::Result;
 
 #[instrument()]
-pub(crate) async fn process(_obj: Arc<Application>) -> Result<Vec<Operation>> {
+pub(crate) async fn process(app: Arc<Application>) -> Result<Vec<Operation>> {
+    let app_name = app.name_any();
+    let namespace = app.namespace().unwrap_or("default".to_string());
+    let labels = BTreeMap::from([
+        ("app.kubernetes.io/name".to_string(), app_name.clone()),
+        ("app.kubernetes.io/managed-by".to_string(), "yakup".to_string()),
+    ]);
     let deployment = Deployment {
         metadata: ObjectMeta {
-            name: Some("test-deployment".to_string()),
-            namespace: Some("default".to_string()),
+            name: Some(app_name.clone()),
+            namespace: Some(namespace.clone()),
             ..Default::default()
         },
         spec: Some(k8s_openapi::api::apps::v1::DeploymentSpec {
             replicas: Some(1),
             selector: LabelSelector {
-                match_labels: Some(
-                    [("app".to_string(), "test".to_string())]
-                        .iter()
-                        .cloned()
-                        .collect(),
-                ),
+                match_labels: Some(labels.clone()),
                 ..Default::default()
             },
             template: PodTemplateSpec {
                 metadata: Some(ObjectMeta {
-                    labels: Some(
-                        [("app".to_string(), "test".to_string())]
-                            .iter()
-                            .cloned()
-                            .collect(),
-                    ),
+                    labels: Some(labels.clone()),
                     ..Default::default()
                 }),
                 spec: Some(k8s_openapi::api::core::v1::PodSpec {
                     containers: vec![k8s_openapi::api::core::v1::Container {
-                        name: "test".to_string(),
-                        image: Some(_obj.spec.image.clone()),
+                        name: app_name.clone(),
+                        image: Some(app.spec.image.clone()),
                         ..Default::default()
                     }],
                     ..Default::default()
