94e6703dbe3f031c7fbbf73495437aa377b395cb
Morten Lied Johansen <mortenjo@ifi.uio.no>
2025-01-24 22:03:36 +0100

    Delete ingresses not in use

--- controller/src/resource_creator/ingress.rs
+++ controller/src/resource_creator/ingress.rs
@@ -1,14 +1,17 @@
-use std::collections::{BTreeMap, HashMap};
+use std::collections::{BTreeMap, HashMap, HashSet};
 use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
 use api::v1::{Application, IngressZone};
 use api::Port;
-use k8s_openapi::api::networking::v1::{HTTPIngressPath, HTTPIngressRuleValue, Ingress, IngressBackend, IngressRule, IngressServiceBackend, IngressSpec, IngressTLS, ServiceBackendPort};
+use k8s_openapi::api::networking::v1::{
+    HTTPIngressPath, HTTPIngressRuleValue, Ingress, IngressBackend, IngressRule,
+    IngressServiceBackend, IngressSpec, IngressTLS, ServiceBackendPort,
+};
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
 use kube::ResourceExt;
+use md5::{Digest, Md5};
 use tracing::instrument;
-use md5::{Md5, Digest};
 
 use crate::models::Operation;
 use crate::resource_creator::to_dynamic_object;
@@ -19,15 +22,25 @@ pub(crate) fn process(
     zones: &HashMap<String, Arc<IngressZone>>,
     object_meta: ObjectMeta,
 ) -> Result<Vec<Operation>> {
+    let mut possible_ingresses: HashSet<String> =
+        HashSet::from_iter(zones.keys().map(|k| format!("{}-{}", app.name_any(), k)));
+
     let ingresses: Vec<Ingress> = app
         .spec
         .ports
         .iter()
         .filter(|port| !port.ingress.is_empty())
         .flat_map(|port| generate_ingresses(app.clone(), zones, object_meta.clone(), port))
+        .map(|ingress| {
+            let ingress_labels = ingress.metadata.labels.clone().unwrap_or_default();
+            if let Some(zone_name) = ingress_labels.get("yakup.ibidem.no/ingress_zone") {
+                possible_ingresses.remove(&zone_name.clone());
+            }
+            ingress
+        })
         .collect();
 
-    let operations = ingresses
+    let mut operations: Vec<Operation> = ingresses
         .iter()
         .map(|ingress| -> Result<Operation> {
             let dynamic_object = to_dynamic_object(ingress.clone())?;
@@ -44,6 +57,20 @@ pub(crate) fn process(
             }
         })
         .collect();
+
+    for ingress_name in possible_ingresses {
+        let delete_ingress = Ingress {
+            metadata: ObjectMeta {
+                name: Some(ingress_name),
+                namespace: object_meta.namespace.clone(),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        let dynamic_object = to_dynamic_object(delete_ingress)?;
+        operations.push(Operation::DeleteIfExists(Arc::new(dynamic_object)));
+    }
+
     Ok(operations)
 }
 
@@ -80,32 +107,44 @@ fn generate_ingress(
 
     let host = zone.spec.host.replace("{appname}", app.name_any().as_str());
 
-    let paths = ingress.paths.iter()
-        .map(|path| {
-            HTTPIngressPath {
-                backend: IngressBackend {
-                    resource: None,
-                    service: Some(IngressServiceBackend {
-                        name: app.name_any(),
-                        port: Some(ServiceBackendPort {
-                            name: Some("http".to_string()),
-                            number: None,
-                        }),
+    let paths = ingress
+        .paths
+        .iter()
+        .map(|path| HTTPIngressPath {
+            backend: IngressBackend {
+                resource: None,
+                service: Some(IngressServiceBackend {
+                    name: app.name_any(),
+                    port: Some(ServiceBackendPort {
+                        name: Some("http".to_string()),
+                        number: None,
                     }),
-                },
-                path: Some(path.clone()),
-                path_type: ingress.path_type.clone().unwrap_or_default().to_string(),
-            }
+                }),
+            },
+            path: Some(path.clone()),
+            path_type: ingress.path_type.clone().unwrap_or_default().to_string(),
         })
         .collect();
 
     object_meta.name = Some(format!("{}-{}", app.name_any(), zone.name_any()));
+    if let Some(labels) = &mut object_meta.labels {
+        labels.insert(
+            "yakup.ibidem.no/ingress_zone".to_string(),
+            zone.name_any().clone(),
+        );
+    } else {
+        object_meta.labels = Some(BTreeMap::from([(
+            "yakup.ibidem.no/ingress_zone".to_string(),
+            zone.name_any().clone(),
+        )]));
+    }
 
     let tls = match &zone.spec.tls {
         Some(zone_tls) => {
-            object_meta.annotations = Some(BTreeMap::from([
-                ("cert-manager.io/cluster-issuer".to_string(), zone_tls.cluster_issuer.clone().unwrap_or_default()),
-            ]));
+            object_meta.annotations = Some(BTreeMap::from([(
+                "cert-manager.io/cluster-issuer".to_string(),
+                zone_tls.cluster_issuer.clone().unwrap_or_default(),
+            )]));
             let hosts_md5 = Md5::digest(host.clone().as_bytes());
             let hosts_id = fast32::base32::CROCKFORD_LOWER.encode(&hosts_md5);
             Some(vec![IngressTLS {
@@ -122,9 +161,7 @@ fn generate_ingress(
             ingress_class_name: zone.spec.ingress_class.clone(),
             rules: Some(vec![IngressRule {
                 host: Some(host.clone()),
-                http: Some(HTTPIngressRuleValue {
-                    paths,
-                })
+                http: Some(HTTPIngressRuleValue { paths }),
             }]),
             tls,
             ..Default::default()
